0faaeacec7cbd8361c3ff05f989398f7cfeaade9
rajasekarkayamboo <67813395+rajasekarkayamboo@users.noreply.github.com>
2022-06-20 22:53:54 -0400

    Initial Commit

---
+++ src/main/java/com/freddiemac/argodemo/ArgodemoApplication.java
@@ -0,0 +1,13 @@
+package com.freddiemac.argodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgodemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freddiemac/argodemo/ArgodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freddiemac.argodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
